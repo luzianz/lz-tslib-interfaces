0f2c84bc72b105a0ab3da55b7f7d8aa2e21cb88a
Luzian Zagadinow <luzianz@outlook.com>
2015-10-01 02:34:17 -0400

    Deconflate INotifyPropertyChanged by yielding property-changing functionality using INotifyPropertyChanging

--- INotifyPropertyChanged.d.ts
+++ INotifyPropertyChanged.d.ts
@@ -6,10 +6,4 @@ interface INotifyPropertyChanged extends IEventEmitter {
 	
 	/** When a property has changed. */
 	on(event: 'propertyChanged', callback: (propertyName: string) => void);
-	
-	/**
-	 * Before a property might change.
-	 * cancel is provided in the callback if cancellation is supported.
-	 */
-	on(event: 'propertyChanging', callback: (propertyName: string, cancel?: FAction) => void);
 }
